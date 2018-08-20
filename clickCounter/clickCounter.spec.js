d8454fb0c05264ed37b4f4f5f849bfcdc8f1bd99
hanse <hanse@mz.co.kr>
2018-08-20 22:55:11 +0900

    increase 메서드 완료 보이지 않는 부분 작업

--- clickCounter/clickCounter.spec.js
+++ clickCounter/clickCounter.spec.js
@@ -1,12 +1,32 @@
 describe('App.ClickCounter', () => {
 
-  describe('getValue()', () => {
+  let counter;
 
+  // beforeEach: it 함수가 호출 직전에 실행되는 함수
+  beforeEach(() => {
+    counter = App.ClickCounter();
+  });
+
+  describe("increase()", () => {
+    it("카운터를 1 올린다", () => {
+      // 준비
+      // const counter = App.ClickCounter();
+      const iniVal = counter.getValue();
+      // 실행
+      counter.increase();
+
+      // 단언
+      expect(counter.getValue()).toBe(iniVal + 1);
+    });
+  });
+
+  describe('getValue()', () => {
     it('초기값이 0인 카운터 값을 반환한다', () => {
-      const counter = App.ClickCounter();
+      // const counter = App.ClickCounter();
       expect(counter.getValue()).toBe(0);
     });
-
   });
+
+  
 
 });
