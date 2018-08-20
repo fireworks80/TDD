97b1a8cf3a3d42176ecb873f3870d5b419692733
hanse <hanse@mz.co.kr>
2018-08-20 23:02:16 +0900

    clickCounter increase() 추가

--- clickCounter/clickCounter.spec.js
+++ clickCounter/clickCounter.spec.js
@@ -1,9 +1,34 @@
 describe('App.ClickCounter', () => {
 
+  let counter;
+
+  beforeEach(() => {
+    counter = App.ClickCounter();
+  });
+
+  describe('increase()', () => {
+
+    it('값을 1증가 시킨다', () => {
+      // 준비
+      // const counter = App.ClickCounter();
+      // counter의 value의 값이 0이 아닐 수가 있다
+
+      const initValue = counter.getValue();
+
+      // 실행
+      counter.increase();
+
+      // 단언
+      expect(counter.getValue()).toBe(initValue + 1);
+
+    });
+
+  });
+
   describe('getValue()', () => {
 
     it('초기값이 0인 카운터 값을 반환한다', () => {
-      const counter = App.ClickCounter();
+      // const counter = App.ClickCounter();
       expect(counter.getValue()).toBe(0);
     });
 
